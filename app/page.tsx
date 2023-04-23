b4f6642fc7614a33d25fcc31d16800af247f7c87
Michael Frieze <michaelfrieze@icloud.com>
2023-04-23 00:54:52 -0400

    product description page

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,5 @@
-import Stripe from 'stripe';
 import Product from './components/Product';
-
-const getProducts = async () => {
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-    apiVersion: '2022-11-15',
-  });
-
-  const products = await stripe.products.list();
-
-  const productsWithPrices = await Promise.all(
-    products.data.map(async (product) => {
-      const price = await stripe.prices.list({ product: product.id });
-
-      return {
-        id: product.id,
-        name: product.name,
-        price: price.data[0].unit_amount,
-        image: product.images[0],
-        currency: price.data[0].currency,
-      };
-    })
-  );
-
-  return productsWithPrices;
-};
+import getProducts from '@/util/getProducts';
 
 export default async function Home() {
   const products = await getProducts();

5bb3590580a9d0754e7109de4400708e8ad33e1c
Michael Frieze <michaelfrieze@icloud.com>
2023-04-29 01:12:54 -0400

    lottie animations

--- app/components/Checkout.tsx
+++ app/components/Checkout.tsx
@@ -7,6 +7,7 @@ import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import CheckoutForm from './CheckoutForm';
 import { motion } from 'framer-motion';
+import OrderAnimation from './OrderAnimation';
 
 const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
@@ -49,6 +50,7 @@ export default function Checkout() {
 
   return (
     <div>
+      {!clientSecret && <OrderAnimation />}
       {clientSecret && (
         <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
           <Elements options={options} stripe={stripePromise}>

999533bf2abd03ead89f45c3d9d69c2cf2170e96
Michael Frieze <michaelfrieze@icloud.com>
2023-04-29 22:32:00 -0400

    added dropdown to Nav

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Session } from 'next-auth';
-import { signIn } from 'next-auth/react';
+import { signIn, signOut } from 'next-auth/react';
 import Image from 'next/image';
 import Link from 'next/link';
 import Cart from './Cart';
@@ -45,17 +45,48 @@ export default function Nav({ user }: Session) {
         )}
         {/* if the user IS signed in */}
         {user && (
-          <Link href={'/dashboard'}>
-            <li>
+          <li>
+            <div className="dropdown dropdown-end cursor-pointer">
               <Image
                 src={user?.image as string}
                 alt={user.name as string}
                 width={36}
                 height={36}
                 className="rounded-full"
+                tabIndex={0}
               />
-            </li>
-          </Link>
+              <ul
+                tabIndex={0}
+                className="dropdown-content menu p-4 space-y-4 shadow bg-base-100 rounded-box w-72"
+              >
+                <Link
+                  className="hover:bg-base-300 p-4 rounded-md"
+                  href={'/dashboard'}
+                  onClick={() => {
+                    if (document.activeElement instanceof HTMLElement) {
+                      document.activeElement.blur();
+                    }
+                  }}
+                >
+                  Orders
+                </Link>
+                <li
+                  onClick={() => {
+                    signOut({
+                      redirect: true,
+                      callbackUrl: process.env.NEXT_PUBLIC_URL,
+                    });
+                    if (document.activeElement instanceof HTMLElement) {
+                      document.activeElement.blur();
+                    }
+                  }}
+                  className="hover:bg-base-300 p-4 rounded-md"
+                >
+                  Sign out
+                </li>
+              </ul>
+            </div>
+          </li>
         )}
       </ul>
       <AnimatePresence>{cartStore.isOpen && <Cart />}</AnimatePresence>

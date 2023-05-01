29d0abbd9ff7fbc08e49f0932934e6e1dd04f7da
Michael Frieze <michaelfrieze@icloud.com>
2023-04-30 21:05:56 -0400

    adding different fonts and changing logo font style

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -16,7 +16,7 @@ export default function Nav({ user }: Session) {
   return (
     <nav className="flex justify-between items-center py-12">
       <Link href={'/'}>
-        <h1>styled</h1>
+        <h1 className="font-lobster text-xl">Styled</h1>
       </Link>
       <ul className="flex items-center gap-8">
         {/* Toggle the cart */}
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,17 @@ import { getServerSession } from 'next-auth/next';
 import Hydrate from './components/Hydrate';
 import { Roboto, Lobster_Two } from 'next/font/google';
 
-// Define main font
-const roboto = Roboto({ weight: ['400', '500', '700'], subsets: ['latin'] });
+//Define main font
+const roboto = Roboto({
+  weight: ['400', '500', '700'],
+  subsets: ['latin'],
+  variable: '--font-robot',
+});
+const lobster = Lobster_Two({
+  weight: '700',
+  subsets: ['latin'],
+  variable: '--font-lobster',
+});
 
 export const metadata = {
   title: 'Create Next App',
@@ -22,7 +31,7 @@ export default async function RootLayout({
   const session = await getServerSession(authOptions);
 
   return (
-    <html lang="en" className={`${roboto.className}`}>
+    <html className={`${roboto.variable} ${lobster.variable}  `} lang="en">
       <Hydrate>
         <Nav user={session?.user} expires={session?.expires as string} />
         {children}

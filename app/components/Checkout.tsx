748f6a4bc6def7fefbc36003d5210d2cc1eccda6
Michael Frieze <michaelfrieze@icloud.com>
2023-04-30 20:59:22 -0400

    stripe dark mode

--- app/components/Checkout.tsx
+++ app/components/Checkout.tsx
@@ -6,8 +6,9 @@ import { useCartStore } from '@/store';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import CheckoutForm from './CheckoutForm';
-import { motion } from 'framer-motion';
 import OrderAnimation from './OrderAnimation';
+import { motion } from 'framer-motion';
+import { useThemeStore } from '@/store';
 
 const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
@@ -17,9 +18,19 @@ export default function Checkout() {
   const cartStore = useCartStore();
   const router = useRouter();
   const [clientSecret, setClientSecret] = useState('');
+  const themeStore = useThemeStore();
+  const [stripeTheme, setStripeTheme] = useState<
+    'flat' | 'stripe' | 'night' | 'none'
+  >('stripe');
 
   useEffect(() => {
-    // Create a paymentIntent as soon as the page loads
+    //Set the theme of stripe
+    if (themeStore.mode === 'light') {
+      setStripeTheme('stripe');
+    } else {
+      setStripeTheme('night');
+    }
+    //Create a paymentIntent as soon as the page loads up
     fetch('/api/create-payment-intent', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -43,7 +54,7 @@ export default function Checkout() {
   const options: StripeElementsOptions = {
     clientSecret,
     appearance: {
-      theme: 'stripe',
+      theme: stripeTheme,
       labels: 'floating',
     },
   };
--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -5,17 +5,18 @@ import AddCart from './AddCart';
 
 export default async function Product({ searchParams }: SearchParamTypes) {
   return (
-    <div className="flex flex-col justify-between items-center gap-24 text-gray-700 2xl:flex-row">
+    <div className="flex flex-col 2xl:flex-row items-center justify-between gap-16 ">
       <Image
         src={searchParams.image}
         alt={searchParams.name}
         width={600}
         height={600}
-        className="w-full"
-        priority
+        className="w-full rounded-lg"
+        priority={true}
       />
-      <div className="font-medium">
-        <h1 className="text-2xl py-2">{searchParams.name}</h1>
+
+      <div className="font-medium ">
+        <h1 className="text-2xl  py-2">{searchParams.name}</h1>
         <p className="py-2">{searchParams.description}</p>
         <p className="py-2">{searchParams.features}</p>
         <div className="flex gap-2">
